544ad782f079e5eead36f6136256ab95674b424d
Seva <sebastianmauriz@gmail.com>
2020-10-07 20:20:52 -0300

    creacion del proyecto

---
+++ src/test/java/com/example/alumnoseba/AlumnosebaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alumnoseba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlumnosebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
