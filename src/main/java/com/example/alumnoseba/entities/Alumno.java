d9b11368e01e785c45d23e4749ab0d6022ede1c7
Seva <sebastianmauriz@gmail.com>
2020-10-07 21:11:26 -0300

    completo la clase Alumno

--- src/main/java/com/example/alumnoseba/entities/Alumno.java
+++ src/main/java/com/example/alumnoseba/entities/Alumno.java
@@ -1,4 +1,33 @@
 package com.example.alumnoseba.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.envers.Audited;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.util.Date;
+@Entity
+@Table(name = "libro")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Audited
 public class Alumno {
+
+
+    @Column(name = "nombre")
+    private String nombre;
+    @Column(name = "apellido")
+    private String apellido;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "createAr")
+    private Date createAr;
+
+
 }
